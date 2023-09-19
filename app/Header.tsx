de8c94a0854993ad4b9c2576220cd23b2334f3be
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2023-09-19 17:37:36 +0200

    First emmit

---
+++ app/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header '>
+        <a href="./" className='flex m-2 place-content-center'>
+            NOTEBOOK
+        </a>
+        </div>
+  )
+}
+
+export default Header
---
+++ app/Main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl'>main</div>
+  )
+}
+
+export default Main
